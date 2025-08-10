9758577065bf086e3e6e22ab6aa35ec0e689cc0e
kunda <kunda@ADMIN>
2025-08-10 22:00:37 +0530

    added all pages

---
+++ app/(main)/bank-account/details/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BankDetails = () => {
+  return (
+    <div>
+      <h1>Bank Details</h1>
+    </div>
+  );
+};
+
+export default BankDetails;
---
+++ app/(main)/layout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainLayout = ({ children }) => {
+  return <div className="container mx-auto flex ">{children}</div>;
+};
+
+export default MainLayout;
